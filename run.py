885bc3404165c59ede717cb54515779c1d60f685
Chamod Disala <thedisala@yahoo.com>
2025-02-13 21:51:37 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,38 +1,75 @@
 import os
 import subprocess
 import shutil
+import time
 
-CRD_SSH_Code = input("Google CRD SSH Code :")
-username = "user" #@param {type:"string"}
-password = "root" #@param {type:"string"}
+# User details
+username = "user"
+password = "root"
+
+# RustDesk unattended access password
+RUSTDESK_PASSWORD = "thedisala"
+
+# System setup
 os.system(f"useradd -m {username}")
 os.system(f"adduser {username} sudo")
 os.system(f"echo '{username}:{password}' | sudo chpasswd")
 os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
 
-Pin = 123456 #@param {type: "integer"}
-Autostart = True #@param {type: "boolean"}
-
-class CRDSetup:
+class RustDeskSetup:
     def __init__(self, user):
         os.system("apt update")
-        self.installCRD()
+        self.installRustDesk()
+        self.enableUnattendedAccess()
+        self.enableAutostart()
+        self.getRustDeskID()
         self.installDesktopEnvironment()
-        self.changewall()
+        self.changeWallpaper()
         self.installGoogleChrome()
         self.installTelegram()
-        self.installQbit()
-        self.finish(user)
+        self.installQbittorrent()
+        print("\n[+] RustDesk setup complete!")
 
     @staticmethod
-    def installCRD():
-        subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'])
-        subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'])
-        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'])
-        print("Chrome Remoted Desktop Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    def installRustDesk():
+        print("\n[+] Installing RustDesk...")
+        os.system("wget https://github.com/rustdesk/rustdesk/releases/latest/download/rustdesk-deb.zip -O rustdesk.zip")
+        os.system("unzip rustdesk.zip && sudo dpkg -i rustdesk-*.deb")
+        os.system("sudo apt --fix-broken install -y")
+        print("[+] RustDesk installed successfully.")
+
+    @staticmethod
+    def enableUnattendedAccess():
+        print("\n[+] Configuring unattended access for RustDesk...")
+        os.system(f"echo -n '{RUSTDESK_PASSWORD}' | sudo tee /etc/rustdesk/passwd")
+        os.system("sudo systemctl restart rustdesk")
+        print("[+] Unattended access enabled.")
+
+    @staticmethod
+    def enableAutostart():
+        print("\n[+] Setting RustDesk to start on boot...")
+        os.system("mkdir -p ~/.config/autostart")
+        os.system("cp /usr/share/applications/rustdesk.desktop ~/.config/autostart/")
+        os.system("sudo systemctl enable rustdesk")
+        os.system("sudo systemctl start rustdesk")
+        print("[+] RustDesk will now start on boot.")
+
+    @staticmethod
+    def getRustDeskID():
+        print("\n[+] Fetching RustDesk ID...")
+        time.sleep(5)  # Wait for RustDesk to initialize
+        rustdesk_id_file = os.path.expanduser("~/.config/rustdesk/id")
+
+        if os.path.exists(rustdesk_id_file):
+            with open(rustdesk_id_file, "r") as file:
+                rustdesk_id = file.read().strip()
+                print(f"[+] Your RustDesk ID: {rustdesk_id}")
+        else:
+            print("[-] RustDesk ID not found. Ensure RustDesk is running.")
 
     @staticmethod
     def installDesktopEnvironment():
+        print("\n[+] Installing XFCE4 Desktop Environment...")
         os.system("export DEBIAN_FRONTEND=noninteractive")
         os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
         os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
@@ -41,82 +78,43 @@ class CRDSetup:
         os.system("sudo apt purge light-locker")
         os.system("sudo apt install --reinstall xfce4-screensaver")
         os.system("systemctl disable lightdm.service")
-        print("Installed XFCE4 Desktop Environment !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("[+] XFCE4 Desktop Environment installed!")
 
     @staticmethod
     def installGoogleChrome():
+        print("\n[+] Installing Google Chrome...")
         subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"])
         subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"])
         subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'])
-        print("Google Chrome Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    
-    @staticmethod
-    def installTelegram():
-        subprocess.run(["apt", "install", "--assume-yes", "telegram-desktop"])
-        print("Telegram Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("[+] Google Chrome installed!")
 
     @staticmethod
-    def changewall():
+    def installTelegram():
+        print("\n[+] Installing Telegram...")
+        subprocess.run(["apt", "install", "--assume-yes", "telegram-desktop"])
+        print("[+] Telegram installed!")
+
+    @staticmethod
+    def installQbittorrent():
+        print("\n[+] Installing Qbittorrent...")
+        subprocess.run(["sudo", "apt", "update"])
+        subprocess.run(["sudo", "apt", "install", "-y", "qbittorrent"])
+        print("[+] Qbittorrent installed!")
+
+    @staticmethod
+    def changeWallpaper():
+        print("\n[+] Changing desktop wallpaper...")
         os.system(f"curl -s -L -k -o xfce-verticals.png https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/CachedImage_1024_768_POS4.jpg")
         current_directory = os.getcwd()
         custom_wallpaper_path = os.path.join(current_directory, "xfce-verticals.png")
         destination_path = '/usr/share/backgrounds/xfce/'
         shutil.copy(custom_wallpaper_path, destination_path)
-        print("Wallpaper Changed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-   
-    @staticmethod
-    def installQbit():
-        subprocess.run(["sudo", "apt", "update"])
-        subprocess.run(["sudo", "apt", "install", "-y", "qbittorrent"])
-        print("Qbittorrent Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("[+] Wallpaper changed!")
 
-    @staticmethod
-    def finish(user):
-        if Autostart:
-            os.makedirs(f"/home/{user}/.config/autostart", exist_ok=True)
-            link = "www.youtube.com/@The_Disala"
-            colab_autostart = """[Desktop Entry]
-            print("Finalizing !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-Type=Application
-Name=Colab
-Exec=sh -c "sensible-browser {}"
-Icon=
-Comment=Open a predefined notebook at session signin.
-X-GNOME-Autostart-enabled=true""".format(link)
-            with open(f"/home/{user}/.config/autostart/colab.desktop", "w") as f:
-                f.write(colab_autostart)
-            os.system(f"chmod +x /home/{user}/.config/autostart/colab.desktop")
-            os.system(f"chown {user}:{user} /home/{user}/.config")
-            
-        os.system(f"adduser {user} chrome-remote-desktop")
-        command = f"{CRD_SSH_Code} --pin={Pin}"
-        os.system(f"su - {user} -c '{command}'")
-        os.system("service chrome-remote-desktop start")
-        
-        print("..........................................................") 
-        print(".....Brought By The Disala................................") 
-        print("..........................................................") 
-        print("......#####...######...####....####...##.......####.......") 
-        print("......##..##....##....##......##..##..##......##..##......")  
-        print("......##..##....##.....####...######..##......######......") 
-        print("......##..##....##........##..##..##..##......##..##......") 
-        print("......#####...######...####...##..##..######..##..##......") 
-        print("..........................................................") 
-        print("..Youtube Video Tutorial - https://youtu.be/xqpCQCJXKxU ..") 
-        print("..........................................................") 
-        print("Log in PIN : 123456") 
-        print("User Name : user") 
-        print("User Pass : root") 
-        while True:
-            pass
-
+# Start the setup
 try:
-    if CRD_SSH_Code == "":
-        print("Please enter authcode from the given link")
-    elif len(str(Pin)) < 6:
-        print("Enter a pin more or equal to 6 digits")
-    else:
-        CRDSetup(username)
-except NameError as e:
-    print("'username' variable not found, Create a user first")
+    RustDeskSetup(username)
+    while True:
+        pass  # Keeps the script running
+except Exception as e:
+    print(f"[-] An error occurred: {e}")
